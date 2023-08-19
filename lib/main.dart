2542297b2cdbfbbafd3f64c051d9b721ab192fa6
Ismail Jibril <ismail0jibril0>
2023-08-19 16:27:22 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:ecommerce_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      fontFamily: 'Poppins'
+    ),
+    home: HomePage(
+      
+    ),
+  ));
+}
+
